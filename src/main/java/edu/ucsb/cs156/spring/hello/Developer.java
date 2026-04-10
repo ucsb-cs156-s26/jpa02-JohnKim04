557d1deb58cf08fcc4a4a3e804d5da297b252263
JohnKim04 <johnkim6144@gmail.com>
2026-04-09 20:16:05 -0700

    xy - updated with acutal dev name

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -16,11 +16,7 @@ public class Developer {
      */
 
     public static String getName() {
-        // TODO: Change this to your name
-        // You may use just the name that is used on <https://bit.ly/cs156-s26-teams>
-        // i.e. your first name, or your first and initial of last name
-
-        return "Chris G.";
+        return "John K.";
     }
 
     /**
@@ -29,8 +25,8 @@ public class Developer {
      */
 
     public static String getGithubId() {
-        // TODO: Change this to your github id
-        return "cgaucho";
+        // DONE: Change this to your github id
+        return "JohnKim04";
     }
 
     /**
@@ -39,14 +35,14 @@ public class Developer {
      */
     
     public static Team getTeam() {
-        // TODO: Change this to your team name
-        Team team = new Team("s26-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        // DONE: Change this to your team name
+        Team team = new Team("s26-05");
+        team.addMember("Alexia");
+        team.addMember("Elissa");
+        team.addMember("John K.");
+        team.addMember("Joaquin");
+        team.addMember("Soome");
+        team.addMember("Ibrahim");
         return team;
     }
 }
